36f44fdd208c5b5af35d9a4dbb956861a6e74491
javaernesto <javaernesto@github.com>
2021-06-02 10:52:01 +0200

    Added truncation tests [MZ17]

--- comparison.py
+++ comparison.py
@@ -199,20 +199,135 @@ def LTZ(a: int, k: int, kappa: int):
 
     return s
 
+Q = 128 # 1 << 7
+
+def distribute(a: np.ndarray, p=2, mod=Q):
+
+    shares = [0] * p
+    for i in range(1, p):
+        shares[i] = np.random.randint(mod, size=len(a), dtype=int)
+    shares[0] = (a - np.sum(shares[1:])) % mod
+
+    return np.array(shares)
+
+def to_bits(a: int):
+
+    b_str = str(bin(a))[2:]
+    bits = []
+    for b in b_str:
+        bits.append(int(b))
+
+    return np.array(bits)
+
+def random_bits(l: int, p=2):
+    '''
+    Returns m shares of l random bits
+
+    param l: number of bits\\
+    param m: number of players (shares)
+    '''
+
+    r_bits = np.array([np.random.randint(2, dtype=int) for _ in range(l)])
+
+    return distribute(r_bits, p=p, mod=2)
+
+def PRandInt(l: int, mod=Q):
+    '''
+    Returns a random integer of size l bits
+
+    param l: size of random integer
+    '''
+
+    r_bits = np.array([np.random.randint(2, dtype=int) for _ in range(l)])
+    r_int = 0
+    for i, r_i in enumerate(r_bits):
+        r_int += r_i << i
+
+    return r_int
+
+def TruncPr(a: list, k: int, m: int, p=2, kappa=2, mod=Q):
+    '''
+    Probabilistic truncation of secretely shared value a.
+
+    param a:     secretely shared value\\
+    param k:     size of ring Z/2^kZ where values are represented\\
+    param m:     number of bits to truncate to a\\
+    param kappa: security parameter
+    '''
+    b = np.array([1 << (k - 1) + a_i for a_i in a]) % mod
+    r_bits = random_bits(m, p)
+
+    rr = np.zeros(p, dtype=int)
+    for i in range(p):
+        for j in range(m):
+            rr[i] += r_bits[i][j] << j
+    rrr = np.array([PRandInt(kappa + k - m) for _ in range(p)])
+    r = np.array([rr_i << m + rrr_i for rr_i, rrr_i in zip(rr, rrr)]) % mod
+
+    c = sum(b + r) % mod
+    cc = c % (1 << m)
+    aa = np.array([cc - rr_i for rr_i in rr]) % mod
+    d = np.array([((a_i - aa_i) % mod) >> m for a_i, aa_i in zip(a, aa)])
+
+    return d
+
+def get_triples(mod=Q):
+
+    a = np.random.randint(mod, dtype=int)
+    b = np.random.randint(mod, dtype=int)
+    c = (a * b) % mod
+    sh = [distribute(a), distribute(b), distribute(c)]
+
+    return np.array(sh)
+
+def mult(x: list, y: list, m=2):
+
+    triple = get_triples()
+
+''' 
+Implémentation algorithme bipartite de troncature présenté dans
+[MZ17] : https://eprint.iacr.org/2017/396.pdf
+Théorème 1. Implémentation locale pour l'instant. Algorithme probabiliste.
+'''
+
+def TruncL(x: int, d:int, l:int, id:bool) -> int:
+
+    if id:
+        return x >> d
+    else:
+        return (1 << l) - (((1 << l) - x) >> d)
+
 def main():
-        # a = [0, 1, 1, 1, 1, 0]
-        # b = [0, 0, 0, 1, 0, 1]
-        # kappa = 2
-        
-        a = 512 - 13
-        b = 13
-        kappa = 2
+    # a = [0, 1, 1, 1, 1, 0]
+    # b = [0, 0, 0, 1, 0, 1]
+    # kappa = 2
+    
+    # a = 512 - 13
+    # b = 13
+    # kappa = 2
 
-        s1 = LTZ(a, 10, kappa)
-        s2 = LTZ(b, 10, kappa)
+    # s1 = LTZ(a, 10, kappa)
+    # s2 = LTZ(b, 10, kappa)
 
-        print(s1, ", ", s2)
-        print(s1 + s2)
+    # print(s1, ", ", s2)
+    # print(s1 + s2)
+
+    # c = 49
+    # print(to_bits(c))
+
+    # print(distribute([c], p=3))
+    # a = [29, 36, 112] # a = 48
+    # d = TruncPr(a, 7, 2, p=3)
+
+    l = 10 # 2**7 = 128
+    d = 2
+    x = 507
+    r = np.random.randint(1 << l, dtype=int)
+    x_0, x_1 = (x + r, (1 << l) - r)
+    print("Avant x_0, x_1 =", x_0, x_1)
+    t_1, t_2 = TruncL(x_0, d, l, True), TruncL(x_1, d, l, False)
+    print("Après x_0, x_1 =", t_1, t_2)
+    print(((t_1 + t_2) - (1 << l)) in {(x >> d) - 1, (x >> d), (x >> d) + 1})
 
 if __name__ == '__main__':
     main()
